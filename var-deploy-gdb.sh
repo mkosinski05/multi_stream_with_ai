0d7bbfffac2af1c43b8e8f7e92895be8cfa726c5
zkmike <zkmike@gmail.com>
2024-06-10 21:52:46 -0700

    asded commands in debugger script download yolo and tvm files.

--- var-deploy-gdb.sh
+++ var-deploy-gdb.sh
@@ -11,15 +11,16 @@ echo "Deploying to target"
 # kill gdbserver on target and delete old binary
  ssh -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedAlgorithms=+ssh-rsa root@${TARGET_IP} "sh -c '/usr/bin/killall -q gdbserver; rm -rf ${TARGET_DIR}/${PROGRAM}  exit 0'"
 
+ssh -q root@${TARGET_IP} [[ ! -d ${APPDIR} ]] && ssh root@${TARGET_IP} "sh -c 'mkdir ${APPDIR}'" || echo "Directory ${APPDIR} Exits";
+
 # Send additional files
-#ssh -q root@${TARGET_IP} [[ ! -f "labels.txt" ]] && scp labels.txt root@${TARGET_IP}:${TARGET_DIR} || echo "File Exits";
-#ssh -q root@${TARGET_IP} [[ ! -d "yolov2_cam" ]] && scp yolov2_cam.tgz root@${TARGET_IP}:${TARGET_DIR} || echo "File Exits";
-#ssh -q root@${TARGET_IP} [[ ! -d "yolov2_cam" ]] && ssh root@${TARGET_IP} "sh -c 'tar xvf yolov2_cam.tgz; rm -rfd yolov2_cam.tgz'" || echo "File Exits"
+ssh -q root@${TARGET_IP} [[ ! -f "${TARGET_DIR}/${APPDIR}/labels.txt" ]] && scp labels.txt root@${TARGET_IP}:${TARGET_DIR}/${APPDIR} || echo "File Exits";
+ssh -q root@${TARGET_IP} [[ ! -d "${TARGET_DIR}/${APPDIR}/yolov2_cam" ]] && scp yolov2_cam.tgz root@${TARGET_IP}:${TARGET_DIR} || echo "File Exits";
+ssh -q root@${TARGET_IP} [[ ! -d "${TARGET_DIR}/${APPDIR}/yolov2_cam" ]] && ssh root@${TARGET_IP} "sh -c 'tar xvf yolov2_cam.tgz -C ${TARGET_DIR}/${APPDIR}; rm -rfd yolov2_cam.tgz'" || echo "File Exits"
 #ssh -q root@${TARGET_IP} [[ ! -f "/usr/lib64/libtvm_runtime.so" ]] && scp ./runtime/libtvm_runtime.so root@${TARGET_IP}:/usr/lib64;
 
 # send the program to the target
-ssh -q root@${TARGET_IP} [[ ! -d ${APPDIR} ]] && ssh root@${TARGET_IP} "sh -c 'mkdir ${APPDIR}'" || echo "Directory ${APPDIR} Exits";
-scp ./build/${APPDIR} root@${TARGET_IP}:${TARGET_DIR}/${APPDIR}/${PROGRAM}
+scp ./build/${APPDIR} root@${TARGET_IP}:${TARGET_DIR}/${APPDIR}/${APPDIR}
 
 # Must match endsPattern in tasks.json
 echo "Starting GDB Server on Target"
